3a1719842bff8320022fb061af5b5eeb37a3d13c
niemand8080 <ruestungen.suppe.02@icloud.com>
2024-05-25 09:44:27 +0200

    chore: Update extension activation message and publisher name in package.json

--- out/extension.js
+++ out/extension.js
@@ -32,7 +32,7 @@ const vscode = __importStar(require("vscode"));
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "react-component-2" is now active!');
+    console.log('Congratulations :D, your extension "react-component-2" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
@@ -45,6 +45,8 @@ function activate(context) {
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+    console.log('deactivated :C');
+}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
